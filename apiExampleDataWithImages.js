f55a94e571f493824499bad112ca6bd2bf4ea50c
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-04 14:33:10 +0000

    Added new testing data which contains images

---
+++ apiExampleDataWithImages.js
@@ -0,0 +1,214 @@
+const apiExampleDataWithImages = {
+  response: {
+    status: "ok",
+    userTier: "developer",
+    total: 57,
+    startIndex: 1,
+    pageSize: 10,
+    currentPage: 1,
+    pages: 6,
+    orderBy: "newest",
+    results: [
+      {
+        id: "football/live/2023/feb/04/everton-v-arsenal-premier-league-live-score-updates",
+        type: "liveblog",
+        sectionId: "football",
+        sectionName: "Football",
+        webPublicationDate: "2023-02-04T11:52:29Z",
+        webTitle: "Everton v Arsenal: Premier League – live",
+        webUrl:
+          "https://www.theguardian.com/football/live/2023/feb/04/everton-v-arsenal-premier-league-live-score-updates",
+        apiUrl:
+          "https://content.guardianapis.com/football/live/2023/feb/04/everton-v-arsenal-premier-league-live-score-updates",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/a161ccf4f03c3c338a1b9378e39df35aff31d5fa/5_460_5419_3252/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/sport",
+        pillarName: "Sport",
+      },
+      {
+        id: "world/live/2023/feb/04/russia-ukraine-war-live-germany-has-ample-evidence-of-war-crimes-in-ukraine-says-top-prosecutor",
+        type: "liveblog",
+        sectionId: "world",
+        sectionName: "World news",
+        webPublicationDate: "2023-02-04T11:51:47Z",
+        webTitle:
+          "Russia-Ukraine war live: more than 100 soldiers returned to Kyiv in prisoner exchange",
+        webUrl:
+          "https://www.theguardian.com/world/live/2023/feb/04/russia-ukraine-war-live-germany-has-ample-evidence-of-war-crimes-in-ukraine-says-top-prosecutor",
+        apiUrl:
+          "https://content.guardianapis.com/world/live/2023/feb/04/russia-ukraine-war-live-germany-has-ample-evidence-of-war-crimes-in-ukraine-says-top-prosecutor",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/d9e07bc30bb1ff3419fdc1d39ad875757e5f0aaf/148_89_1849_1110/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/news",
+        pillarName: "News",
+      },
+      {
+        id: "uk-news/2023/feb/04/rishi-sunak-stop-deportation-appeals-people-uk-small-boats",
+        type: "article",
+        sectionId: "uk-news",
+        sectionName: "UK news",
+        webPublicationDate: "2023-02-04T11:47:00Z",
+        webTitle:
+          "Sunak ‘aims to stop deportation appeals’ for people who reach UK in small boats",
+        webUrl:
+          "https://www.theguardian.com/uk-news/2023/feb/04/rishi-sunak-stop-deportation-appeals-people-uk-small-boats",
+        apiUrl:
+          "https://content.guardianapis.com/uk-news/2023/feb/04/rishi-sunak-stop-deportation-appeals-people-uk-small-boats",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/72dd82cbbf5624575ed7973992130b5fc221512d/165_115_3830_2299/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/news",
+        pillarName: "News",
+      },
+      {
+        id: "commentisfree/2023/feb/04/want-to-truly-have-empathy-for-animals-stop-owning-pets",
+        type: "article",
+        sectionId: "commentisfree",
+        sectionName: "Opinion",
+        webPublicationDate: "2023-02-04T11:25:00Z",
+        webTitle:
+          "Want to truly have empathy for animals? Stop owning pets | Troy Vettese",
+        webUrl:
+          "https://www.theguardian.com/commentisfree/2023/feb/04/want-to-truly-have-empathy-for-animals-stop-owning-pets",
+        apiUrl:
+          "https://content.guardianapis.com/commentisfree/2023/feb/04/want-to-truly-have-empathy-for-animals-stop-owning-pets",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/d855330e6afe0d8a8be31f1596de079f6f3c1733/165_64_3335_2002/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/opinion",
+        pillarName: "Opinion",
+      },
+      {
+        id: "commentisfree/2023/feb/04/conservatives-ron-desantis-florida-education",
+        type: "article",
+        sectionId: "commentisfree",
+        sectionName: "Opinion",
+        webPublicationDate: "2023-02-04T11:13:00Z",
+        webTitle:
+          "Conservatives angry about school ‘indoctrination’ are telling on themselves | Jan-Werner Müller",
+        webUrl:
+          "https://www.theguardian.com/commentisfree/2023/feb/04/conservatives-ron-desantis-florida-education",
+        apiUrl:
+          "https://content.guardianapis.com/commentisfree/2023/feb/04/conservatives-ron-desantis-florida-education",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/5560d03644edf014bd62cb648d5368af01a04406/0_332_5015_3011/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/opinion",
+        pillarName: "Opinion",
+      },
+      {
+        id: "us-news/2023/feb/04/dreamers-daca-program-under-threat",
+        type: "article",
+        sectionId: "us-news",
+        sectionName: "US news",
+        webPublicationDate: "2023-02-04T11:00:29Z",
+        webTitle:
+          "‘I can’t plan ahead’: Dreamers speak out as US program faces new threat",
+        webUrl:
+          "https://www.theguardian.com/us-news/2023/feb/04/dreamers-daca-program-under-threat",
+        apiUrl:
+          "https://content.guardianapis.com/us-news/2023/feb/04/dreamers-daca-program-under-threat",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/57d6c01707363c21f097c52d0a5884b092f4e6b7/0_482_8192_4918/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/news",
+        pillarName: "News",
+      },
+      {
+        id: "travel/2023/feb/04/10-of-irelands-most-charming-villages",
+        type: "article",
+        sectionId: "travel",
+        sectionName: "Travel",
+        webPublicationDate: "2023-02-04T11:00:29Z",
+        webTitle: " 10 of Ireland’s most charming villages",
+        webUrl:
+          "https://www.theguardian.com/travel/2023/feb/04/10-of-irelands-most-charming-villages",
+        apiUrl:
+          "https://content.guardianapis.com/travel/2023/feb/04/10-of-irelands-most-charming-villages",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/c80af0824cb35efa3bfdbd8524546a01adbb2e4d/0_315_8256_4954/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/lifestyle",
+        pillarName: "Lifestyle",
+      },
+      {
+        id: "us-news/2023/feb/04/california-bald-eagles-nest-tribal-communities-activists-pacific-gas-electric",
+        type: "article",
+        sectionId: "us-news",
+        sectionName: "US news",
+        webPublicationDate: "2023-02-04T11:00:28Z",
+        webTitle:
+          "Two bald eagles nested in a pine for years. A utility company tried to chop it down",
+        webUrl:
+          "https://www.theguardian.com/us-news/2023/feb/04/california-bald-eagles-nest-tribal-communities-activists-pacific-gas-electric",
+        apiUrl:
+          "https://content.guardianapis.com/us-news/2023/feb/04/california-bald-eagles-nest-tribal-communities-activists-pacific-gas-electric",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/f0572d4744de28eeb1d7fb8f11787b507909fa1e/0_197_1787_1072/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/news",
+        pillarName: "News",
+      },
+      {
+        id: "uk-news/2023/feb/04/sister-nicola-bulley-says-no-evidence-fell-river",
+        type: "article",
+        sectionId: "uk-news",
+        sectionName: "UK news",
+        webPublicationDate: "2023-02-04T10:50:53Z",
+        webTitle:
+          "Sister of Nicola Bulley says there is ‘no evidence’ she fell into river",
+        webUrl:
+          "https://www.theguardian.com/uk-news/2023/feb/04/sister-nicola-bulley-says-no-evidence-fell-river",
+        apiUrl:
+          "https://content.guardianapis.com/uk-news/2023/feb/04/sister-nicola-bulley-says-no-evidence-fell-river",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/5d4a555c86e6d825864996373f4ac6f98efec619/0_206_3000_1801/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/news",
+        pillarName: "News",
+      },
+      {
+        id: "lifeandstyle/2023/feb/04/its-a-control-thing-why-are-we-so-fascinated-by-super-organised-homes",
+        type: "article",
+        sectionId: "lifeandstyle",
+        sectionName: "Life and style",
+        webPublicationDate: "2023-02-04T10:00:28Z",
+        webTitle:
+          "‘It’s a control thing’: why are we so fascinated by super-organised homes? ",
+        webUrl:
+          "https://www.theguardian.com/lifeandstyle/2023/feb/04/its-a-control-thing-why-are-we-so-fascinated-by-super-organised-homes",
+        apiUrl:
+          "https://content.guardianapis.com/lifeandstyle/2023/feb/04/its-a-control-thing-why-are-we-so-fascinated-by-super-organised-homes",
+        fields: {
+          thumbnail:
+            "https://media.guim.co.uk/e59f171faad5d9dcf3572f3465a7f3d8b86d08d2/0_422_9000_5400/500.jpg",
+        },
+        isHosted: false,
+        pillarId: "pillar/lifestyle",
+        pillarName: "Lifestyle",
+      },
+    ],
+  },
+};
+
+module.exports = apiExampleDataWithImages;
