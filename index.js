0e2bc9c8f15e9b70d96f00b0a54664844cac3bb6
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-04 11:37:25 +0000

    Website now automatically loads and displays data from API when first visited

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const NewsClient = require("./newsClient");
+const NewsModel = require("./newsModel");
+const NewsView = require("./newsView");
 
 client = new NewsClient();
+model = new NewsModel();
+view = new NewsView(model, client);
+view.displayNewsFromApi();
