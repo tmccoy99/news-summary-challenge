467e274bdd91aea8611443392ba54485648c8dbd
Thomas McCoy <finnmccoy99@gmail.com>
2023-02-04 14:43:55 +0000

    Updated all test to use new test data

--- newsView.test.js
+++ newsView.test.js
@@ -4,7 +4,6 @@
 const fs = require("fs");
 require("jest-fetch-mock").enableMocks();
 const NewsView = require("./newsView");
-const apiExampleData = require("./apiExampleData");
 const apiExampleDataWithImages = require("./apiExampleDataWithImages");
 
 describe("Testing display of the day's news", () => {
@@ -25,21 +24,25 @@ describe("Testing display of the day's news", () => {
   });
 
   it("With news set in model, displayNews() adds div.headline elements to the page", () => {
-    mockModel.getNews.mockReturnValueOnce(apiExampleData.response.results);
+    mockModel.getNews.mockReturnValueOnce(
+      apiExampleDataWithImages.response.results
+    );
     newsView.displayNews();
     expect(document.querySelector("div.headline")).not.toBeNull();
   });
 
   it("With news set in model, displayNews() adds div.headline element with correct textContent for each headline", () => {
-    mockModel.getNews.mockReturnValueOnce(apiExampleData.response.results);
+    mockModel.getNews.mockReturnValueOnce(
+      apiExampleDataWithImages.response.results
+    );
     newsView.displayNews();
     const headlines = document.querySelectorAll("div.headline");
     expect(headlines.length).toBe(10);
     expect(headlines[1].textContent).toBe(
-      "Russia-Ukraine war live: EU to set up centre to prosecute crimes committed in Ukraine; UK rejects Johnson’s call to send fighter jets"
+      "Russia-Ukraine war live: more than 100 soldiers returned to Kyiv in prisoner exchange"
     );
     expect(headlines[2].textContent).toBe(
-      "King Kazu’s astonishing longevity and a new move to Portugal"
+      "Sunak ‘aims to stop deportation appeals’ for people who reach UK in small boats"
     );
   });
 
@@ -55,37 +58,26 @@ describe("Testing display of the day's news", () => {
     );
   });
 
-  // it("With news set in model, displayNews() headline elements are hyperlinks", () => {
-  //   mockModel.getNews.mockReturnValueOnce(
-  //     apiExampleDataWithImages.response.results
-  //   );
-  //   newsView.displayNews();
-  //   const headlines = document.querySelectorAll("div.headline");
-  //   expect(headlines.length).toBe(10);
-  //   expect(
-  //     (headlines[0].href =
-  //       "https://www.theguardian.com/football/live/2023/feb/04/everton-v-arsenal-premier-league-live-score-updates")
-  //   );
-  // });
-
   it("displayNewsFromApi() method loads data from API then displays headlines", () => {
     mockClient.loadTodaysHeadlines.mockImplementationOnce((callback) =>
-      callback(apiExampleData.response.results)
+      callback(apiExampleDataWithImages.response.results)
     );
 
-    mockModel.getNews.mockReturnValueOnce(apiExampleData.response.results);
+    mockModel.getNews.mockReturnValueOnce(
+      apiExampleDataWithImages.response.results
+    );
     newsView.displayNewsFromApi().then(() => {
       const headlines = document.querySelectorAll("div.headline");
       expect(headlines.length).toBe(10);
       expect(headlines[1].textContent).toBe(
-        "Russia-Ukraine war live: EU to set up centre to prosecute crimes committed in Ukraine; UK rejects Johnson’s call to send fighter jets"
+        "Russia-Ukraine war live: more than 100 soldiers returned to Kyiv in prisoner exchange"
       );
       expect(headlines[2].textContent).toBe(
-        "King Kazu’s astonishing longevity and a new move to Portugal"
+        "Sunak ‘aims to stop deportation appeals’ for people who reach UK in small boats"
       );
       expect(mockClient.loadTodaysHeadlines).toHaveBeenCalled();
       expect(mockModel.setNews).toHaveBeenCalledWith(
-        apiExampleData.response.results
+        apiExampleDataWithImages.response.results
       );
     });
   });
